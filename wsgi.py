42ba2ea30130ab52c65f03cb89fe4867c46b8279
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2020-10-08 02:54:29 +0000

    Change the struct

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
-import sys
+import sys,os
 # insert at position 1 in the path, as 0 is the path of this file.
-sys.path.insert(0, 'C:/Users/user/important/Projet EDX/project1/blog')
-
+sys.path.append(os.getcwd()+'/blog')
+print(sys.path)
 from app import app
 
 if __name__ == "__main__":
