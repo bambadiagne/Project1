2d73867515a8bfff72aa8b73479f2ae8d2e71b29
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2020-10-08 02:04:02 +0000

    Add file wsgi

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import sys
 # insert at position 1 in the path, as 0 is the path of this file.
 sys.path.insert(0, 'C:/Users/user/important/Projet EDX/project1/blog')
 
-from application import app
+from app import app
 
 if __name__ == "__main__":
     app.run()
