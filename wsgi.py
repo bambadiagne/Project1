d6ceee0f041fefef35a2f8a082b6132232a382ef
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2020-10-09 00:02:44 +0000

    Upgrade the design

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 import sys,os
-# insert at position 1 in the path, as 0 is the path of this file.
+
 sys.path.append(os.getcwd()+'/blog')
-print(sys.path)
 from app import app
 
 if __name__ == "__main__":
